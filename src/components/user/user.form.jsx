52aa128d1ab54c488e791ea25baebe3e8f27066a
DGW-HoangDM <hoang-dm@dgw.asia>
2024-07-09 11:58:57 +0700

    user axios create User

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,30 +1,75 @@
-import { Input } from "antd";
-import { Col, Row } from 'antd';
-import { Button, Flex } from 'antd';
-import { Space, Typography } from 'antd';
+import { Input, notification, Col, Row, Button, Flex,Space, Typography } from "antd";
 const { Text, Link } = Typography;
 
+import { useState } from "react";
+import { createUserAPI } from "../../services/api.service";
+
 const UserForm = () => {
-  return(
-    <div style={{display: "flex", gap: 20, flexDirection: "column", padding: 20}}>
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleCreateUser = async () => {
+    const res = await createUserAPI(fullName, email, password, phone);
+    if(res.data && res.data.data){
+      notification.success({
+        message: "Create user",
+        description: "Tạo user thành công"
+      })
+    }
+  };
+
+  return (
+    <div
+      style={{ display: "flex", gap: 20, flexDirection: "column", padding: 20 }}
+    >
       <Row>
         <Text>Full name</Text>
-        <Input placeholder="Fulname" />
+        <Input
+          placeholder="fullName"
+          value={fullName}
+          onChange={(e) => {
+            setFullName(e.target.value);
+          }}
+        />
       </Row>
       <Row>
         <Text>Email</Text>
-        <Input placeholder="Email" />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
       </Row>
       <Row>
         <Text>Password</Text>
-        <Input.Password placeholder="Password" type="password" />
+        <Input.Password
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </Row>
       <Row>
         <Text>Phone number</Text>
-        <Input placeholder="Phone number" type="number" />
+        <Input
+          placeholder="Phone number"
+          type="number"
+          value={phone}
+          onChange={(e) => {
+            setPhone(e.target.value);
+          }}
+        />
       </Row>
       <Row>
-        <Button type="primary">Create user</Button>
+        <Button type="primary" onClick={handleCreateUser}>
+          Create user
+        </Button>
       </Row>
     </div>
   );
