a2c46ed3d88d203d6df553fe6655d9c86c9ee70a
DGW-HoangDM <hoang-dm@dgw.asia>
2024-07-16 13:56:27 +0700

    video #82 show avatar

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -22,8 +22,8 @@ const ViewUserDetail = (props) => {
       }}
       open={isDetailUserOpen}
     >
-      {
-        dataDetailUser ? <>
+      {dataDetailUser ? (
+        <>
           <p style={{ display: "flex", gap: "20px", marginBottom: 10 }}>
             <TeamOutlined />
             <span>{dataDetailUser.fullName}</span>
@@ -32,17 +32,43 @@ const ViewUserDetail = (props) => {
             <MailOutlined />
             <span>{dataDetailUser.email}</span>
           </p>
-          <p style={{ display: "flex", gap: "20px" }}>
+          <p style={{ display: "flex", gap: "20px", marginBottom: 10 }}>
             <WhatsAppOutlined />
             <span>{dataDetailUser.phone}</span>
           </p>
+          <p>Avatar:</p>
+          <div>
+            <img
+              height={150}
+              width={150}
+              src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${
+                dataDetailUser.avatar
+              }`}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="btnUpload"
+              style={{
+                display: "block",
+                width: "fit-content",
+                marginTop: "15px",
+                padding: "5px 10px",
+                background: "orange",
+                borderRadius: "5px",
+                cursor: "pointer",
+              }}
+            >
+              Upload Avatar
+            </label>
+            <input type="file" hidden id="btnUpload" />
+          </div>
         </>
-        :
+      ) : (
         <>
           <p>Không có dữ liệu</p>
         </>
-      }
-      
+      )}
     </Drawer>
   );
 };
